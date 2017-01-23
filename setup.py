a6e0c05ca8f0e526e8d80221ab5d6c182dec7a9d
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2017-01-23 00:01:16 -0200

    Commit inicial

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+
+version = __import__('mediafeed').__version__
+with open('README.rst', 'rb') as f:
+    long_description = f.read().decode('utf-8')
+
+
+setup(
+    name='MediaFeed',
+    version=version,
+    packages=find_packages(),
+
+    author='Eduardo Klosowski',
+    author_email='eduardo_klosowski@yahoo.com',
+
+    description='Download de arquivos de fonte de mídias',
+    long_description=long_description,
+    license='MIT',
+    url='https://github.com/eduardoklosowski/mediafeed',
+)
