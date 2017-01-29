98dcf23c9ee2bee81caa0be6265e4f1abdf016a2
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2017-01-29 01:55:58 -0200

    Adicionado biblioteca para dados

--- mediafeed/__init__.py
+++ mediafeed/__init__.py
@@ -10,7 +10,9 @@ logger = getLogger('mediafeed')
 def init():
     from .commands import load_commands
     from .modules import load_modules
+    from .databases import initdb
 
     logger.info('Iniciando Media Feed')
     load_modules()
     load_commands()
+    initdb()
--- mediafeed/settings.py
+++ mediafeed/settings.py
@@ -4,4 +4,6 @@ import os
 DATA_PATH = os.environ.get('MEDIAFEED_DATA', os.path.expanduser(os.path.join('~', '.mediafeed', 'data')))
 os.makedirs(DATA_PATH, exist_ok=True)
 
+DATABASE_URL = 'sqlite:///%s/db.sqlite3' % DATA_PATH
+
 DAEMON_PORT = int(os.environ.get('MEDIAFEED_PORT', 5447))
