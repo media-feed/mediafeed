434970ed36ed8f0fc40ad8d797d0e7c382b2ab9e
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2017-08-07 03:06:36 -0300

    Adicionado CORS para a API

--- mediafeed/api/server.py
+++ mediafeed/api/server.py
@@ -1,12 +1,19 @@
 import os
 
-from bottle import Bottle
+from bottle import Bottle, response
 from whitenoise import WhiteNoise
 
 from ..settings import DATA_PATH
 
 
 api = Bottle()
+
+
+@api.hook('after_request')
+def enable_cors():
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, DELETE'
+
 
 application = WhiteNoise(api)
 application.add_files(os.path.join(DATA_PATH, 'thumbnails'), prefix='thumbnails/')
